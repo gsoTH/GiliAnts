1f291094e3bdf974a0c1edd2fda792ce486183f7
Marcus Vinicius Thomas <TH@gsokoeln.onmicrosoft.com>
2021-03-31 20:41:20 +0200

    Ant kann erstellt werden und kann sich in zufälliger Richtung bewegen.

---
+++ GiliAnts/Ant.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Drawing;
+
+namespace GiliAnts
+{
+    public class Ant
+    {
+        Random rnd = new Random();
+        private Point position;
+
+        public Ant(int x, int y)
+        {
+            this.position = new Point(x, y);
+        }
+
+        public void Move()
+        {
+            int step = 1;
+            int i = rnd.Next(0, 3);
+            
+            switch (i)
+            {
+                case 0: //move north
+                    this.position.Y -= step;
+                    break;
+                case 1: //move east
+                    this.position.X += step;
+                    break;
+                case 2: //move south
+                    this.position.Y += step;
+                    break;
+                case 3: //move west
+                    this.position.X -= step;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public Point Position
+        {
+            get => position;
+        }
+    }
+}
