dff87e06784e0de0807e70ee1ed3af2068bd859e
Marcus Vinicius Thomas <TH@gsokoeln.onmicrosoft.com>
2021-04-01 00:17:44 +0200

    Bei Bewegungen sollten Pheromone verstreut werden...

---
+++ GiliAnts/Pheromone.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Drawing;
+
+namespace GiliAnts
+{
+    public struct Pheromone
+    {
+        private Point position;
+        private int intensity;
+
+        public Pheromone(Point position, int intensity)
+        {
+            Position = position;
+            Intensity = intensity;
+        }
+
+        public Pheromone(Point position) : this(position, 1) { }
+
+        public Point Position { get => position; set => position = value; }
+        public int Intensity { get => intensity; set => intensity = value; }
+    }
+}
